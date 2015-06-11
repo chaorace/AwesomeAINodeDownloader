97417c1b61fa3b09ea8ed68dd11e7e6a5a4f6f2f
chaorace <chaorace@gmail.com>
2015-06-11 06:21:32 -0400

    Generated nodes are now all child of a single easily copied block

--- src/main/java/AwesomeXmlFormatter.scala
+++ src/main/java/AwesomeXmlFormatter.scala
@@ -4,11 +4,11 @@
 object AwesomeXmlFormatter {
   def formatNodes(nodes: Array[String], rule: String) = {
     //build output
-    var output = "<?xml version=\"1.0\" ?>\n<enemy>\n    <behaviour>\n        <root x=\"110\" y=\"40\">\n            <string id=\"Comment\">Map nodes</string>\n            <normal>\n"
+    var output = "<?xml version=\"1.0\" ?>\n<enemy>\n    <behaviour>\n        <root x=\"110\" y=\"40\">\n            <string id=\"Comment\">MAPNAME Map nodes</string>\n            <normal>\n                <sequence>\n                    <string id=\"Is blocking\" values=\"yesno\">no</string>\n                    <normal>"
     nodes.foreach(x => {
       output += rule.replaceAll("NODENAME", x) + "\n"
     })
-    output += "</normal>\n        </root>\n    </behaviour>\n</enemy>"
+    output += "</normal>\n                </sequence>\n            </normal>\n        </root>\n    </behaviour>\n</enemy>"
     //return output
     output
   }
